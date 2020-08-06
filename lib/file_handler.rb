0309ce1cc40d50cbad673d3bd3782e253d2740e6
Jacobus-afk <s24162826@gmail.com>
2020-08-06 22:23:23 +0200

    working save functionality

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -15,7 +15,8 @@ end
 
 # class to handle dictionary
 class DictionaryHandler < FileHandler
-  attr_reader :guess_word
+  # attr_reader :guess_word
+  attr_accessor :guess_word
   def initialize(filename)
     super(filename)
     @guess_word = generate_word
@@ -28,24 +29,45 @@ class DictionaryHandler < FileHandler
       rand(@line_count).times { @raw_file.gets }
       word = $LAST_READ_LINE.chomp
     end
-    word
+    word.downcase
   end
 end
 
 # class to handle save files
 class SaveFileHandler < FileHandler
+  attr_reader :saved_game
   def initialize
-    puts
+    @saved_game = nil
+    Dir.mkdir(SAVEPATH) unless Dir.exist? SAVEPATH
+    print 'Load a save file (Y/N)? '
+    load_game if gets.chomp.downcase == 'y'
   end
 
   def save_game(game)
-    Dir.mkdir(SAVEPATH) unless Dir.exist? SAVEPATH
     system('cls') || system('clear')
-    print 'Name for savefile? '
-    filename = gets.chomp + '.yml'
+    print 'Name for save file? '
+    filename = gets.chomp.downcase + '.yml'
     # test = YAML.dump(game.to_h)
     File.open(SAVEPATH + filename, 'w') { |file| file.write(game.to_yaml) }
     puts 'Game saved.. Press Enter'
     gets
+  end
+
+  private
+
+  def load_game
+    save_files = Dir.entries(SAVEPATH).filter_map { |file| file.split('.').first unless File.directory? file }
+    puts "List of save_files: #{save_files}"
+    loop do
+      print 'Please type in the name of your save file: '
+      save_selection = gets.chomp.downcase
+      return read_file(save_selection) if save_files.include? save_selection
+
+      puts 'Invalid save file name'
+    end
+  end
+
+  def read_file(save_selection)
+    @saved_game = YAML.load(File.read(SAVEPATH + save_selection + '.yml'))
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,38 @@ require_relative 'file_handler.rb'
 
 # class for all the different game functionality
 class Game
-  attr_reader :turns_left, :guessed_correctly, :dict_word
+  attr_reader :turns_left, :guessed_correctly, :dict_word, :used_letters, :correct_guessed_chars, :unlocked_word, :guess
   def initialize
     @saver = SaveFileHandler.new
     @dictionary = DictionaryHandler.new('5desk.txt')
+
+    if @saver.saved_game
+      load_saved_game
+    else
+      load_defaults
+    end
+    # @dict_word = @dictionary.guess_word
+    # @turns_left = 8
+    # @used_letters = []
+    # @correct_guessed_chars = []
+    # @guessed_correctly = false
+    # @unlocked_word = generate_word_dashes
+    # @guess = ''
+  end
+
+  def play_round
+    system('cls') || system('clear')
+    puts "Your word: #{@unlocked_word} \t\t"\
+        "Turns left: #{@turns_left} \t"\
+        "Used letters: #{@used_letters.join(',')}\t\t"\
+        "Press \` to save game, ESC to exit \n\n"
+    acquire_guess
+    handle_guess
+  end
+
+  private
+
+  def load_defaults
     @dict_word = @dictionary.guess_word
     @turns_left = 8
     @used_letters = []
@@ -17,29 +45,39 @@ class Game
     @guess = ''
   end
 
-  def play_round
-    system('cls') || system('clear')
-    puts "Your word: #{@unlocked_word} \t\t"\
-        "Turns left: #{@turns_left} \t\t"\
-        "Used letters: #{@used_letters.join(',')}\n\n"
-    acquire_guess
-    handle_guess
+  def load_saved_game
+    # @dictionary = DictionaryHandler.new('5desk.txt')
+    @dictionary.guess_word = @saver.saved_game.dict_word
+    @dict_word = @saver.saved_game.dict_word
+    @turns_left = @saver.saved_game.turns_left
+    @used_letters = @saver.saved_game.used_letters
+    @correct_guessed_chars = @saver.saved_game.correct_guessed_chars
+    @guessed_correctly = @saver.saved_game.guessed_correctly
+    @unlocked_word = @saver.saved_game.unlocked_word
+    @guess = @saver.saved_game.guess
   end
 
-  private
+  def handle_input
+    system('stty raw -echo') #=> Raw mode, no echo
+    @guess = STDIN.getc
+    system('stty -raw echo') #=> Reset terminal mode
+    return @saver.save_game(self) if @guess == "\`"
+
+    return @turns_left = 0 if @guess.ord == 27
+
+    true if (@guess =~ /[a-z]/) && !@used_letters.include?(@guess)
+  end
 
   def acquire_guess
     loop do
       print 'Your guess? '
       # https://stackoverflow.com/questions/946738/detect-key-press-non-blocking-w-o-getc-gets-in-ruby/22659929
-      system('stty raw -echo') #=> Raw mode, no echo
-      @guess = STDIN.getc
-      system('stty -raw echo') #=> Reset terminal mode
-      # @guess = gets.chomp.downcase
-      @saver.save_game(self) if @guess == "\`"
-      next unless @guess =~ /[a-z]/
+      break if handle_input == true
 
-      next if @used_letters.include? @guess
+      # @guess = gets.chomp.downcase
+      # next unless @guess =~ /[a-z]/
+
+      # next if @used_letters.include? @guess
 
       return
     end
